7f107f8c3950351c1efb95bd3b573f2969410e04
Ксения <kseniyakr2006@gmail.com>
2025-11-29 12:10:49 +0300

    create parse method, succed creation

--- XmlParser23/src/main/java/org/example/Main.java
+++ XmlParser23/src/main/java/org/example/Main.java
@@ -1,17 +1,62 @@
 package org.example;
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.ParseException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        parseSonnetFromFile("fileToParse.xml");
+    }
+    public static void parseSonnetFromFile(String xmlFilePath) {
+        try {
+            // xml parser
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            // parsing from file
+            Document document = builder.parse(new File(xmlFilePath));
+            document.getDocumentElement().normalize();
+
+            // data for name of file from tags
+            String firstName = getTagValue("firstName", document);
+            String lastName = getTagValue("lastName", document);
+            String title = getTagValue("title", document);
+
+            // get all lines
+            NodeList lineNodes = document.getElementsByTagName("line");
+            StringBuilder linesContent = new StringBuilder();
+
+            //append all lines, where every line starts from new str
+            for (int i = 0; i < lineNodes.getLength(); i++) {
+                String line = lineNodes.item(i).getTextContent();
+                linesContent.append(line).append("\n");
+            }
+
+            // name of file
+            String filename = firstName + "_" + lastName + "_" + title + ".txt";
+
+            // write in file
+            Files.write(Paths.get(filename), linesContent.toString().getBytes());
+
+            System.out.println("Create file: " + filename);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static String getTagValue(String tagName, Document document) throws ParseException{
+        NodeList nodeList = document.getElementsByTagName(tagName);
+        if (nodeList.getLength() > 0) {
+            return nodeList.item(0).getTextContent();
+        }
+        else{
+            throw new ParseException("No tags found",0);
         }
     }
 }
