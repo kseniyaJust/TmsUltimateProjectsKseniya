b7c69a8d388e70b3f17be42c5407dad729ca169d
Ксения <kseniyakr2006@gmail.com>
2026-01-26 20:21:53 +0300

    exceptions handler

---
+++ hobbyCatalog/src/main/java/org/example/hobbycatalog/exceptions/HobbyNotFoundException.java
@@ -0,0 +1,6 @@
+package org.example.hobbycatalog.exceptions;
+
+public class HobbyNotFoundException extends RuntimeException {
+
+        public HobbyNotFoundException(String message) { super(message); }
+}
--- hobbyCatalog/src/main/java/org/example/hobbycatalog/exceptions/ItemNotFoundExceptions.java
+++
@@ -1,4 +0,0 @@
-package org.example.hobbycatalog.exceptions;
-
-public class ItemNotFoundExceptions {
-}
