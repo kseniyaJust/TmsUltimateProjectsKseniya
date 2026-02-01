ab32cb7cd0fc31c1b59d367f8f41ada4d7cf6752
Ксения <kseniyakr2006@gmail.com>
2026-02-01 12:12:36 +0300

    method patterns for controller

--- hobbyCatalog/src/main/java/org/example/hobbycatalog/controller/UserAdressController.java
+++ hobbyCatalog/src/main/java/org/example/hobbycatalog/controller/UserAdressController.java
@@ -1,4 +1,40 @@
 package org.example.hobbycatalog.controller;
 
+import org.example.hobbycatalog.DTO.UserAdressDTO;
+import org.example.hobbycatalog.entity.UserAdress;
+import org.example.hobbycatalog.entity.Wallet;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/adress")
 public class UserAdressController {
+
+    @GetMapping
+    public String getAllAdresses(){
+        return "get all adresses";
+    }
+
+    @PostMapping
+    public String addAdress(@RequestBody UserAdressDTO userAdress){
+        return  "add adress";
+    }
+
+    //adresses can be updated or deleted only by owner user
+    @PutMapping
+    public String updateAdress(@RequestBody UserAdressDTO userAdress){
+        return "update adress";
+    }
+
+    @DeleteMapping
+    public String deleteAdress(@RequestParam Long id_adress){
+        return "delete adress";
+    }
 }
--- hobbyCatalog/src/main/java/org/example/hobbycatalog/controller/WalletController.java
+++ hobbyCatalog/src/main/java/org/example/hobbycatalog/controller/WalletController.java
@@ -1,4 +1,43 @@
 package org.example.hobbycatalog.controller;
 
+import org.example.hobbycatalog.DTO.WalletDTO;
+import org.example.hobbycatalog.entity.Wallet;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wallet")
 public class WalletController {
+
+    @GetMapping
+    private String getBalance(){
+        return "balance";
+    }
+
+    @PutMapping
+    private String putMoney(){
+        return "money";
+    }
+
+    @PostMapping
+    private String addWallet(@RequestBody WalletDTO wallet){
+        return "new wallet";
+    }
+
+    //wallet can be deleted or updated only by owner user
+    @PutMapping
+    private String updateWallet(@RequestBody WalletDTO wallet){
+        return "update wallet";
+    }
+
+    @DeleteMapping
+    private String deleteWallet(@RequestParam Long id_wallet){
+        return "delete wallet";
+    }
 }
