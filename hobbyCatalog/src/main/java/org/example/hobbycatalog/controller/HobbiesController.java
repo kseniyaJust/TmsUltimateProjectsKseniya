59cc3e9bc2538ec27e12d1fe30513fbdbd94ac42
Ксения <kseniyakr2006@gmail.com>
2026-03-04 20:31:17 +0300

    some changes in update of hobby and creating

--- hobbyCatalog/src/main/java/org/example/hobbycatalog/controller/HobbiesController.java
+++ hobbyCatalog/src/main/java/org/example/hobbycatalog/controller/HobbiesController.java
@@ -1,10 +1,14 @@
 package org.example.hobbycatalog.controller;
 
+import jakarta.validation.Valid;
+import org.example.hobbycatalog.DTO.UpdateHobbyDTO;
+import org.example.hobbycatalog.entity.Hobbies;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/catalog")
@@ -20,12 +24,12 @@ public class HobbiesController {
     }
 
     @PostMapping
-    public String createHobby(){
+    public String createHobby(@Valid Hobbies hobbies){
         return "create";
     }
 
     @PutMapping("/{id}")
-    public String changeHobby(@PathVariable Long id){
+    public String changeHobby(@PathVariable Long id, @RequestBody @Valid UpdateHobbyDTO updateHobbyDTO){
         return "change";
     }
 
