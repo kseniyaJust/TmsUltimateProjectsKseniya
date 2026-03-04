d509afffc3494c3db3883da0192d37b8392e176b
Ксения <kseniyakr2006@gmail.com>
2026-03-04 20:30:29 +0300

    swagger documentation full

--- hobbyCatalog/src/main/java/org/example/hobbycatalog/swagger/UserAdressSwagger.java
+++ hobbyCatalog/src/main/java/org/example/hobbycatalog/swagger/UserAdressSwagger.java
@@ -12,38 +12,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface UserAdressSwagger {
 
     @Operation(summary = "Method that gives a list of all existing addresses of current user")
-    @ApiResponse(responseCode = "200", description = "")
-    @ApiResponse(responseCode = "400", description = "")
-    @ApiResponse(responseCode = "403", description = "")
-    @ApiResponse(responseCode = "409", description = "")
-    @ApiResponse(responseCode = "500", description = "")
-    @Parameter(name = "",description = "")
+    @ApiResponse(responseCode = "200", description = "All addresses are displayed successfully")
+    @ApiResponse(responseCode = "403", description = "Unauthorised user")
+    @ApiResponse(responseCode = "409", description = "Database conflict")
+    @ApiResponse(responseCode = "500", description = "Server error")
     public String getAllAdresses();
 
     @Operation(summary = "Method that allow to create new address of current user")
-    @ApiResponse(responseCode = "200", description = "")
-    @ApiResponse(responseCode = "400", description = "")
-    @ApiResponse(responseCode = "403", description = "")
-    @ApiResponse(responseCode = "409", description = "")
-    @ApiResponse(responseCode = "500", description = "")
-    @Parameter(name = "",description = "")
-    public String addAdress(@RequestBody UserAdressDTO userAdress);
+    @ApiResponse(responseCode = "200", description = "New address wa created successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid dto was given")
+    @ApiResponse(responseCode = "403", description = "Unauthorised user")
+    @ApiResponse(responseCode = "409", description = "Database conflict")
+    @ApiResponse(responseCode = "500", description = "Server error")
+    @Parameter(name = "Adress.class",description = "Class for creating address")
+    public String addAdress();
 
     @Operation(summary = "Method that allow to update 1 address for given id of current user")
-    @ApiResponse(responseCode = "200", description = "")
-    @ApiResponse(responseCode = "400", description = "")
-    @ApiResponse(responseCode = "403", description = "")
-    @ApiResponse(responseCode = "409", description = "")
-    @ApiResponse(responseCode = "500", description = "")
-    @Parameter(name = "",description = "")
+    @ApiResponse(responseCode = "200", description = "Address was updated successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid dto was given")
+    @ApiResponse(responseCode = "403", description = "Unauthorised user")
+    @ApiResponse(responseCode = "409", description = "Database conflict")
+    @ApiResponse(responseCode = "500", description = "Server error")
+    @Parameter(name = "UserAddressUpdateDTO",description = "DTO for updating address")
     public String updateAdress(@RequestBody UserAdressDTO userAdress);
 
     @Operation(summary = "Method that allow to delte 1 address for given id of current user")
-    @ApiResponse(responseCode = "200", description = "")
-    @ApiResponse(responseCode = "400", description = "")
-    @ApiResponse(responseCode = "403", description = "")
-    @ApiResponse(responseCode = "409", description = "")
-    @ApiResponse(responseCode = "500", description = "")
-    @Parameter(name = "",description = "")
+    @ApiResponse(responseCode = "200", description = "Address was deleted successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid dto was given")
+    @ApiResponse(responseCode = "403", description = "Unauthorised user")
+    @ApiResponse(responseCode = "409", description = "Database conflict")
+    @ApiResponse(responseCode = "500", description = "Server error")
+    @Parameter(name = "Long id",description = "Id of address to delete")
     public String deleteAdress(@RequestParam Long id_adress);
 }
--- hobbyCatalog/src/main/java/org/example/hobbycatalog/swagger/UserSwagger.java
+++ hobbyCatalog/src/main/java/org/example/hobbycatalog/swagger/UserSwagger.java
@@ -9,29 +9,27 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 public interface UserSwagger {
 
     @Operation(summary = "Method, that register new user")
-    @ApiResponse(responseCode = "200", description = "")
-    @ApiResponse(responseCode = "400", description = "")
-    @ApiResponse(responseCode = "403", description = "")
-    @ApiResponse(responseCode = "409", description = "")
-    @ApiResponse(responseCode = "500", description = "")
-    @Parameter(name = "",description = "")
+    @ApiResponse(responseCode = "200", description = "User registered successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid data dto given")
+    @ApiResponse(responseCode = "409", description = "Database conflict")
+    @ApiResponse(responseCode = "500", description = "Server error")
+    @Parameter(name = "AuthRequestDTO",description = "DTO for user registration")
     public String registUser();
 
     @Operation(summary = "Method that log in existing user")
-    @ApiResponse(responseCode = "200", description = "")
-    @ApiResponse(responseCode = "400", description = "")
-    @ApiResponse(responseCode = "403", description = "")
-    @ApiResponse(responseCode = "409", description = "")
-    @ApiResponse(responseCode = "500", description = "")
-    @Parameter(name = "",description = "")
+    @ApiResponse(responseCode = "200", description = "User log in successfully")
+    @ApiResponse(responseCode = "400", description = "No user found")
+    @ApiResponse(responseCode = "409", description = "Database conflict")
+    @ApiResponse(responseCode = "500", description = "Server error")
+    @Parameter(name = "LoginRequestDTO",description = "DTO for login")
     public String loginUser();
 
     @Operation(summary = "Method that refresh token to continue current user session")
-    @ApiResponse(responseCode = "200", description = "")
-    @ApiResponse(responseCode = "400", description = "")
-    @ApiResponse(responseCode = "403", description = "")
-    @ApiResponse(responseCode = "409", description = "")
-    @ApiResponse(responseCode = "500", description = "")
-    @Parameter(name = "",description = "")
+    @ApiResponse(responseCode = "200", description = "Session was activated successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid login dto given")
+    @ApiResponse(responseCode = "403", description = "Session is expired")
+    @ApiResponse(responseCode = "409", description = "Database conflict")
+    @ApiResponse(responseCode = "500", description = "Server error")
+    @Parameter(name = "LoginRequestDTO",description = "DTO with login fields for refresh tokens")
     public String refreshToken();
 }
